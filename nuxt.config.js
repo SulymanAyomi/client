a8893d6f60350a2d194d4f76a982e5a782288b3c
Sulyman Ayomi <Sulymanayomi@gmail.com>
2022-10-10 11:05:09 +0100

    fx

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     Proxy: true,
-    baseURL: "http://localhost:4000",
+    baseURL: URL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
